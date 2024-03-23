9986d358979b0c39364d9af6df2c3a26e660dde7
artiehumphreys <artieh2003@gmail.com>
2024-03-23 13:45:03 -0400

    Added simple speech to text function

--- src/speechtotext.py
+++ src/speechtotext.py
@@ -2,21 +2,20 @@ import speech_recognition as sr
 
 def speech_to_text():
     recognizer = sr.Recognizer()
-    mic = sr.Microphone()
+    recognizer.pause_threshold = 0.8
+    
+    with sr.Microphone() as mic:
+        print("Listening...")
+        recognizer.adjust_for_ambient_noise(mic)
+        
+        try:
+            audio = recognizer.listen(mic)
+            text = recognizer.recognize_google(audio)
+            print("You said: " + text)
+            
+        except sr.UnknownValueError:
+            print("Google Web Speech API could not understand the audio")
+        except sr.RequestError as e:
+            print(f"Could not request results from Google Web Speech API; {e}")
 
-    with mic as source:
-        print("Say something...")
-        recognizer.adjust_for_ambient_noise(source)  # Adjust for ambient noise
-        audio = recognizer.listen(source)
-
-    try:
-        print("Recognizing...")
-        text = recognizer.recognize_google(audio)  # Google Web Speech API
-        print("You said:", text)
-    except sr.UnknownValueError:
-        print("Sorry, could not understand audio.")
-    except sr.RequestError as e:
-        print("Could not request results from Google Web Speech API; {0}".format(e))
-
-if __name__ == "__main__":
-    speech_to_text()
+speech_to_text()
